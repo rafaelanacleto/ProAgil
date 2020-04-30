ba61dc0ef49550d390f5344639361b2ca53c4dba
rafael anacleto <rafael.ferreira.anacleto@gmail.com>
2020-04-30 12:05:25 -0300

    first commit

---
+++ ProAgil.API/Controllers/EventosController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+        public DataContext Context { get; }
+
+        public EventosController(DataContext context)
+        {
+            this.Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+               return Ok(await Context.Eventos.ToListAsync()); 
+            }
+            catch (System.Exception)
+            {                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro Interno API");
+            }
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+               var results = await Context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+               return Ok(results); 
+            }
+            catch (System.Exception)
+            {                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro Interno API");
+            }
+            
+        }
+
+    }
+}
