082bbe60e76a503237ffd87b0714cdd90ab3e591
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2020-07-28 15:01:30 -0300

    config baseURL auth service

--- ProAgil-App/src/app/_services/auth.service.ts
+++ ProAgil-App/src/app/_services/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+baseURL = 'http://localhost:5000/eventos';
+
+constructor(private http: HttpClient) { }
+
+
 
 }
