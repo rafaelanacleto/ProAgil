eec4727d7fdf137d33f1c4854868fcb0714bd1a2
Rafael Anacleto <rafael.anacleto@cwi.com.br>
2020-07-28 10:45:46 -0300

    criado register login e nav bar auth

--- ProAgil-App/src/app/user/registration/registration.component.ts
+++ ProAgil-App/src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.validation();
   }
+
+  validation() {
+    this.registerForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(25)]],      
+      email  : ['', [Validators.required, Validators.email]],            
+      userName: ['', Validators.required],      
+      passwords : this.fb.group({
+        password : ['', [Validators.required, Validators.minLength(4)]],           
+        confirmPassword: ['', Validators.required]
+      }, {validator: this.compararSenhas})
+      
+    });
+  }
+
+  compararSenhas(fb: FormGroup) {
+    const confirmarSenhaCtrl = fb.get('confirmPassword');
+    if (confirmarSenhaCtrl.errors == null || 'mismatch' in confirmarSenhaCtrl.errors) {
+      if (fb.get('password').value !== confirmarSenhaCtrl.value) {
+        fb.get('confirmPassword').setErrors({mismatch: true});
+      }else{
+        fb.get('confirmPassword').setErrors(null);
+      }
+    }
+  }
+
+  cadastrarUsuario() {
+    console.log('cadastrar user');
+  }
+
+
 
 }
